f744e34d9142d2a695f218e28fef487999c86791
am-tropin <94761432+am-tropin@users.noreply.github.com>
2023-05-29 16:04:16 +0400

    added stopwords into recommenders

--- lyrics_recommender.py
+++ lyrics_recommender.py
@@ -4,14 +4,6 @@
 # In[1]:
 
 
-
-
-
-# In[1]:
-
-
-import os
-import re
 import pandas as pd
 
 
@@ -22,24 +14,22 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-# # 1. Choosing the song for comparison
+# In[16]:
+
+
+import nltk
+stopwords = nltk.corpus.stopwords.words('english')
+
 
 # In[3]:
 
 
-# printing a list of titles in lexicographic order with division by the first letter
+# import sys
+# sys.path.append('../')
+from functions.function_store import print_lexic_order_with_first_letter, collect_artist_list, collect_song_list, collect_full_discography
 
-def print_lexic_order_with_first_letter(list_list):
-    list_list = sorted(list_list)
-    first_letter = ''
-    for s in list_list:
-        if s[0] != first_letter:
-            first_letter = s[0]
-            print()
-            print(str.upper(first_letter))
-        print(s)
-    return 1
 
+# # 1. Choosing the song for comparison
 
 # In[4]:
 
@@ -48,9 +38,8 @@ def print_lexic_order_with_first_letter(list_list):
 To view the collection
 '''
 
-path = "discography/"
-dir_list = os.listdir(path)
-artist_list = [re.sub("_discography_allthelyrics.csv", "", d) for d in dir_list if "_discography_allthelyrics.csv" in d]
+# path = "discography/"
+artist_list = collect_artist_list()
 
 print_lexic_order_with_first_letter(artist_list)
 
@@ -77,23 +66,14 @@ target_artist = input()
 # In[6]:
 
 
-discography_df = pd.read_csv('discography/' + target_artist + '_discography_allthelyrics.csv', sep='\t', encoding='utf-8')
-discography_df.head()
-
-
-# In[7]:
-
-
 '''
 To view the discography
 '''
 
-songs = list(discography_df['song'])
-
-print_lexic_order_with_first_letter(songs)
+print_lexic_order_with_first_letter(collect_song_list(target_artist))
 
 
-# In[8]:
+# In[7]:
 
 
 '''
@@ -103,7 +83,7 @@ To choose the song:
 target_song = 'No Light No Light'
 
 
-# In[40]:
+# In[15]:
 
 
 # print()
@@ -111,65 +91,34 @@ target_song = 'No Light No Light'
 # target_song = input()
 
 
-# # 2. Creating the full lyrics base
-
-# In[9]:
+# In[8]:
 
 
-full_discography_list = []
+# for user-based recommender
 
-for artist in artist_list:
-    discography_df = pd.read_csv('discography/' + artist + '_discography_allthelyrics.csv', sep='\t', encoding='utf-8')
-    discography_df['artist'] = artist
-    discography_df['key'] = discography_df.apply(lambda x: x['artist'] + '__' + '_'.join(x['album'].split()) + '__' + '_'.join(x['song'].split()), axis=1)
-# .strip(",[]()-?!'")
-    full_discography_list.append(discography_df)
-    
-full_discography_df = pd.concat(full_discography_list, axis=0)[['key', 'artist', 'album', 'song', 'lyrics', 'link']]
-# full_discography_df
-    
-
-
-# In[10]:
-
-
-full_discography_df2 = full_discography_df.set_index('key')
-full_discography_df2.head()
-
-
-# In[11]:
-
-
-target_lyrics_df = full_discography_df2[
-    (full_discography_df2['artist'] == target_artist) &
-#     full_discography_df2[''] == album_title &
-    (full_discography_df2['song'] == target_song)
+target_songs_list = [
+    ['queen', "Don't Stop Me Now"],
+    ['queen', "Under Pressure"],
+    ['lana_del_rey', "Blue Jeans"],
+    ['rainbow', "Stargazer"],
+    ['guns_n_roses', "You Could Be Mine"]
 ]
 
-target_key = target_lyrics_df.index[0]
-target_key
+
+# # 2. Text-based Recommender
+
+# In[17]:
 
 
-# In[ ]:
-
-
-
-
-
-# # 3. Text-based Recommender
-
-# In[12]:
-
-
-def to_recommend_lyrics(df, target_key):
+def to_recommend_lyrics(df, column, target_key):
     # Instantiate the vectorizer object and transform the plot column
-    vectorizer = TfidfVectorizer(max_df=0.7, min_df=2)
-    vectorized_data = vectorizer.fit_transform(df['lyrics'])
+    vectorizer = TfidfVectorizer(max_df=0.7, min_df=2, stop_words=stopwords)
+    vectorized_data = vectorizer.fit_transform(df[column])
 
     # Create Dataframe from TF-IDFarray
     tfidf_df = pd.DataFrame(vectorized_data.toarray(), columns=vectorizer.get_feature_names_out())
 
-    # Assign the movie titles to the index and inspect
+    # Assign the song titles to the index and inspect
     tfidf_df.index = df.index
 
     # Create the array of cosine similarity values
@@ -178,7 +127,7 @@ def to_recommend_lyrics(df, target_key):
     # Wrap the array in a pandas DataFrame
     cosine_similarity_df = pd.DataFrame(cosine_similarity_array, index=tfidf_df.index, columns=tfidf_df.index)
 
-    # Find the values for the movie Rio
+    # Find the values for the target song
     cosine_similarity_series = cosine_similarity_df.loc[target_key]
 
     # Sort these values highest to lowest
@@ -187,28 +136,7 @@ def to_recommend_lyrics(df, target_key):
     return ordered_similarities
 
 
-
-# In[13]:
-
-
-def top_of_similarity(similar_df, threshold):
-    similar_df_head = similar_df[similar_df.values >= threshold]
-    similar_df_head2 = pd.DataFrame(similar_df_head)
-    similar_df_head2.columns = ['similarity_score']
-    
-    return similar_df_head2
-
-
-# In[14]:
-
-
-similar_df = to_recommend_lyrics(full_discography_df2, target_key)
-
-similar_df_head2 = top_of_similarity(similar_df, 0.2)
-similar_df_head2.join(full_discography_df2).reset_index(drop=True)[['similarity_score','artist','album','song']]
-
-
-# In[37]:
+# In[18]:
 
 
 def top_recommended_lyrics(artist, song, n):
@@ -223,88 +151,47 @@ def top_recommended_lyrics(artist, song, n):
     Returns:
         DataFrame
     """
-    target_lyrics_df = full_discography_df2[
+    full_discography_df2 = collect_full_discography()
+    target_key = full_discography_df2[
         (full_discography_df2['artist'] == artist) &
         (full_discography_df2['song'] == song)
-    ]
-    target_key = target_lyrics_df.index[0]
-    similar_df = pd.DataFrame(to_recommend_lyrics(full_discography_df2, target_key)).rename(columns={target_key:'similarity_score'})
+    ].index[0] # select only one key in case of song duplicates
+    
+    similar_df = pd.DataFrame(to_recommend_lyrics(full_discography_df2, 'lyrics', target_key)).rename(columns={target_key:'similarity_score'})
     similar_df2 = similar_df.join(full_discography_df2).reset_index(drop=True)[['similarity_score','artist','album','song']]
 
     return similar_df2[similar_df2.index.isin(list(range(1, n+1)))]
 
 
-# In[38]:
+# In[19]:
 
 
 top_recommended_lyrics(target_artist, target_song, 5)
 
 
-# # 4. User-profile Recommender
+# # 3. User-profile Recommender
 
-# In[ ]:
-
-
-'''
-define the set of your favourite songs
-and 
-learn the most similar songs from the rest of the full discography
-'''
+# In[20]:
 
 
-# In[79]:
-
-
-my_favourite_songs_list = [
-    ['queen', "", "Don't Stop Me Now"],
-    ['queen', "", "Under Pressure"],
-    ['lana_del_rey', "", "Blue Jeans"],
-    ['rainbow', "", "Stargazer"],
-    ['guns_n_roses', "", "You Could Be Mine"]
-]
-
-
-# In[82]:
-
-
-my_favourite_songs_df = pd.DataFrame(my_favourite_songs_list)
-my_favourite_songs_df.columns = ['artist', 'album_0', 'song']
-my_favourite_songs_df = my_favourite_songs_df.drop(['album_0'], axis=1)
-my_favourite_songs_df
-
-
-# In[102]:
-
-
-df3 = full_discography_df2.merge(my_favourite_songs_df, how='inner', on=['artist', 'song'])
-df3['key'] = df3.apply(lambda x: x['artist'] + '__' + '_'.join(x['album'].split()) + '__' + '_'.join(x['song'].split()), axis=1)
-df3 = df3.set_index('key')
-# df3.index
-
-my_favourite_songs = list(df3.index)
-my_favourite_songs
-
-
-# In[107]:
-
-
-def to_recommend_lyrics_by_set(df, target_list):
-    vectorizer = TfidfVectorizer(max_df=0.7, min_df=2)
-    vectorized_data = vectorizer.fit_transform(df['lyrics'])
+def to_recommend_lyrics_by_set(df, column, target_list):
+    # Instantiate the vectorizer object and transform the plot column
+    vectorizer = TfidfVectorizer(max_df=0.7, min_df=2, stop_words=stopwords)
+    vectorized_data = vectorizer.fit_transform(df[column])
 
     # Create Dataframe from TF-IDFarray
     tfidf_df = pd.DataFrame(vectorized_data.toarray(), columns=vectorizer.get_feature_names_out())
 
-    # Assign the movie titles to the index and inspect
+    # Assign the song titles to the index and inspect
     tfidf_df.index = df.index
+    
+    # Create a subset of only the songs the user has enjoyed
+    songs_enjoyed_df = tfidf_df.reindex(target_list)
 
-    # Create a subset of only the movies the user has enjoyed
-    movies_enjoyed_df = tfidf_df.reindex(target_list)
+    # Generate the user profile by finding the average scores of songs they enjoyed
+    user_prof = songs_enjoyed_df.mean()
 
-    # Generate the user profile by finding the average scores of movies they enjoyed
-    user_prof = movies_enjoyed_df.mean()
-
-    # Find subset of tfidf_df that does not include movies in list_of_movies_enjoyed
+    # Find subset of tfidf_df that does not include songs in target_list
     tfidf_subset_df = tfidf_df.drop(target_list, axis=0)
 
     # Calculate the cosine_similarity and wrap it in a DataFrame
@@ -317,20 +204,40 @@ def to_recommend_lyrics_by_set(df, target_list):
     return sorted_similarity_df
 
 
-
-# In[109]:
-
-
-similar_by_set_df = to_recommend_lyrics_by_set(full_discography_df2, my_favourite_songs)
-
-similar_df_head3 = top_of_similarity(similar_by_set_df, 0.3)
-similar_df_head3.join(full_discography_df2).reset_index(drop=True)[['similarity_score','artist','album','song']]
+# In[21]:
 
 
-# In[ ]:
+def top_recommended_lyrics_by_set(user_list, n):
+    """Returns list of Top-`n` song lyrics which are most similar to songs 
+    with artist name and song name from `user_list`.
+    
+    Args:
+        user_list (list): 
+        n (int): The number of most similar songs.
+    
+    Returns:
+        DataFrame
+    """
+    full_discography_df2 = collect_full_discography()
+    my_favourite_songs = []
+
+    for row in user_list:
+        target_key = full_discography_df2[
+            (full_discography_df2['artist'] == row[0]) &
+            (full_discography_df2['song'] == row[1])
+        ].index[0] # select only one key in case of song duplicates
+        my_favourite_songs.append(target_key)
+    
+    similar_df = pd.DataFrame(to_recommend_lyrics_by_set(full_discography_df2, 'lyrics', my_favourite_songs)).rename(columns={target_key:'similarity_score'})
+    similar_df2 = similar_df.join(full_discography_df2).reset_index(drop=True)[['similarity_score','artist','album','song']]
+
+    return similar_df2[similar_df2.index.isin(list(range(n)))]
 
 
+# In[22]:
 
+
+top_recommended_lyrics_by_set(target_songs_list, 5)
 
 
 # In[ ]:
